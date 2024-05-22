b9ff123389537c4eb79974233df8a7f74ae46a80
pratyushchauhan147 <121055922+pratyushchauhan147@users.noreply.github.com>
2024-05-22 14:45:28 +0530

    Update settings.py

--- backend/cores/settings.py
+++ backend/cores/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fgnht4$&m10h(9vfo-j0dbykoe(+$k$$6g!na*wlyg0!wev3+y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
